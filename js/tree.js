4cff03e88f67c9562b5c3c4b1407098ea08b8eef
Darryl Hein <dhein@xmmedia.com>
2012-08-04 21:50:07 -0600

    completing the base of the CSS and JS for the tree

--- js/tree.js
+++ js/tree.js
@@ -1,5 +1,13 @@
 $(function() {
-	$('.tree .expand, .tree .collapse').click(function(e) {
+	var $tree = $('.tree');
+
+	$('.add_item').click(function(e) {
+		e.preventDefault();
+
+		var $dialog = open_dialog('Add Item');
+	});
+
+	$tree.delegate('.expand, .collapse', 'click', function(e) {
 		e.preventDefault();
 
 		var $link = $(this),
@@ -14,4 +22,38 @@ $(function() {
 			$link.removeClass('collapse').addClass('expand');
 		}
 	});
+
+	$tree.delegate('.edit_item', 'click', function(e) {
+		e.preventDefault();
+
+		var $dialog = open_dialog('Edit Item');
+	});
+
+	$tree.delegate('.delete_item', 'click', function(e) {
+		e.preventDefault();
+
+		var $dialog = open_dialog('Delete Item');
+	});
+
+	$tree.delegate('.add_sub_item', 'click', function(e) {
+		e.preventDefault();
+
+		var $dialog = open_dialog('Add Sub Item');
+	});
 });
+
+var open_dialog = function(title) {
+	return $('#tree_dialog').dialog({
+		title : title,
+		autoOpen : true,
+		height: 400,
+		minHeight: 400,
+		width: 450,
+		minWidth: 450,
+		buttons: {
+			'Close': function() {
+				$(this).dialog('close');
+			}
+		}
+	}).html('<img src="/images/loading.gif"> Loading...');
+}
