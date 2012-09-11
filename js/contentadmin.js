2bceab40397574d9539ff1369f45de6d4ff8648a
Darryl Hein <dhein@xmmedia.com>
2012-09-11 13:55:03 -0600

    adding the CSS & JS for the content admin

---
+++ js/contentadmin.js
@@ -0,0 +1,72 @@
+$(function() {
+	// on change of the compare to select, submit the form, refreshing the page
+	$('.js_compare_to').on('change', function() {
+		$(this).closest('form').submit();
+	});
+
+	// show hide the content changes in the content admin diff view
+	$('.js_content_admin_hide_changes').on('click', function(e) {
+		e.preventDefault();
+
+		var bkg_colour,
+			link = $(this);
+
+		if ($('.diff-html-removed').css('display') == 'none') {
+			bkg_colour = '#82ff6e';
+			link.html('Hide Changes');
+		} else {
+			bkg_colour = '#fff';
+			link.html('Show Changes');
+		}
+
+		$('.diff-html-added').css('background-color', '#fff');
+		$('.diff-html-removed').toggle();
+	});
+
+	// show hide the full content area
+	$('.js_content_admin_show_content').on('click', function(e) {
+		e.preventDefault();
+
+		var link = $(this),
+			full_content = $('.content_diff_all[rel="' + link.attr('rel') + '"]');
+		full_content.slideToggle(function() {
+			if ($(this).is(':visible')) {
+				link.html('Hide ' + link.data('name'));
+			} else {
+				link.html('Show ' + link.data('name'));
+			}
+		});
+	});
+
+	// view the content of the history item
+	$('.content_history').on('click', '.js_history_view', function(e) {
+		e.preventDefault();
+
+		var dialog = $('#js_content_admin_dialog').dialog({
+			title : 'View Version',
+			modal : true,
+			width : 750,
+			height : 500,
+			buttons : {
+				Close : function() {
+					$(this).dialog('close');
+				}
+			}
+		}).html('<img src="/images/loading.gif"> Loading...');
+
+		$.getJSON($(this).attr('href'), function(return_data) {
+			if (cl4.process_ajax(return_data)) {
+				dialog.html(return_data.html);
+			} else {
+				dialog.html(return_data.error_msg);
+				setTimeout("$('#js_content_admin_dialog').dialog('close');", 2000);
+			}
+		});
+
+	// restore a past history item
+	}).on('click', '.js_restore', function(e) {
+		if ( ! confirm('Are you sure you want to restore this version?')) {
+			e.preventDefault();
+		}
+	});
+});
---
+++ js/contentadmin_public.js
@@ -0,0 +1,16 @@
+var content_admin_edit = null;
+
+$(function() {
+	$('.contentadmin_edit').on('click', function(e) {
+		e.preventDefault();
+		if (content_admin_edit == null || content_admin_edit.closed) {
+			content_admin_edit = window.open($(this).attr('href'), 'content_admin_edit', 'width=900,height=750,location=yes,resizable=yes,scrollbars=yes,status=yes,dependent=yes');
+			if (content_admin_edit == null) {
+				alert('There was a problem opening the edit window. Please use the content admin page to change the content.');
+			}
+		} else {
+			content_admin_edit.location.href = $(this).attr('href');
+			content_admin_edit.focus();
+		}
+	});
+});
