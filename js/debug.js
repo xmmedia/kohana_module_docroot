7b0ffc4f4f211a434595eefae39a8fafdfe96f10
Darryl Hein <dhein@xmmedia.com>
2012-09-14 18:43:53 -0600

    adding JS debug file

---
+++ js/debug.js
@@ -0,0 +1,50 @@
+// Avoid `console` errors in browsers that lack a console.
+if (!(window.console && console.log)) {
+	(function() {
+		var noop = function() {};
+		var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+		var length = methods.length;
+		var console = window.console = {};
+		while (length--) {
+			console[methods[length]] = noop;
+		}
+	}());
+}
+
+var xm_debug = {
+	/**
+	 * Function : dump()
+	 * Arguments: The data - array,hash(associative array),object
+	 *    The level - OPTIONAL
+	 * Returns  : The textual representation of the array.
+	 * This function was inspired by the print_r function of PHP.
+	 * This will accept some data as the argument and return a
+	 * text that will be a more readable version of the
+	 * array/hash/object that is given.
+	 * Docs: http://www.openjs.com/scripts/others/dump_function_php_print_r.php
+	 */
+	dump : function(arr, level) {
+		var dumped_text = "";
+		if(!level) level = 0;
+
+		//The padding given at the beginning of the line.
+		var level_padding = "";
+		for(var j=0;j<level+1;j++) level_padding += "    ";
+
+		if(typeof(arr) == 'object') { //Array/Hashes/Objects
+			for(var item in arr) {
+				var value = arr[item];
+
+				if(typeof(value) == 'object') { //If it is an array,
+					dumped_text += level_padding + "'" + item + "' ...\n";
+					dumped_text += dump(value,level+1);
+				} else {
+					dumped_text += level_padding + "'" + item + "' => \"" + value + "\"\n";
+				}
+			}
+		} else { //Stings/Chars/Numbers etc.
+			dumped_text = "===>"+arr+"<===("+typeof(arr)+")";
+		}
+		return dumped_text;
+	}
+};
